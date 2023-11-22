be0c6a5ebd441b6842182639d157b6296e072885
Brian Ndilo <b_munguti@yahoo.com>
2023-11-22 01:34:24 -0800

    You need to test it

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,76 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.place import Place
+"""test for city"""
+import os
+import unittest
+
+import pep8
+
+from models.base_model import BaseModel
+from models.city import City
 
 
-class test_Place(test_basemodel):
-    """ """
+class TestCity(unittest.TestCase):
+    """this will test the city class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.city = City()
+        cls.city.name = "LA"
+        cls.city.state_id = "CA"
 
-    def test_city_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.city_id), str)
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.city
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_pep8_city(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        pep = style.check_files(["models/city.py"])
+        self.assertEqual(pep.total_errors, 0, "fix pep8")
 
-    def test_description(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.description), str)
+    def test_checking_for_docstring_city(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(City.__doc__)
 
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+    def test_attributes_city(self):
+        """chekcing if City have attributes"""
+        self.assertTrue("id" in self.city.__dict__)
+        self.assertTrue("created_at" in self.city.__dict__)
+        self.assertTrue("updated_at" in self.city.__dict__)
+        self.assertTrue("state_id" in self.city.__dict__)
+        self.assertTrue("name" in self.city.__dict__)
 
-    def test_number_bathrooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+    def test_is_subclass_city(self):
+        """test if City is subclass of Basemodel"""
+        self.assertTrue(issubclass(self.city.__class__, BaseModel), True)
 
-    def test_max_guest(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.max_guest), int)
+    def test_attribute_types_city(self):
+        """test attribute type for City"""
+        self.assertEqual(type(self.city.name), str)
+        self.assertEqual(type(self.city.state_id), str)
 
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+    @unittest.skipIf(
+        os.getenv("HBNB_TYPE_STORAGE") == "db",
+        "This test only work in Filestorage",
+    )
+    def test_save_city(self):
+        """test if the save works"""
+        self.city.save()
+        self.assertNotEqual(self.city.created_at, self.city.updated_at)
 
-    def test_latitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+    def test_to_dict_city(self):
+        """test if dictionary works"""
+        self.assertEqual("to_dict" in dir(self.city), True)
 
-    def test_longitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
 
-    def test_amenity_ids(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+if __name__ == "__main__":
+    unittest.main()
